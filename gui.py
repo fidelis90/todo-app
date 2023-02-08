cfaf57211ad04bd25412ae7c787772b68a794a5d
fidelis90 <fidelisogunsanmi@gmail.com>
2023-02-07 21:23:04 -0500

    implement exit and remove buttons

--- gui.py
+++ gui.py
@@ -8,9 +8,12 @@ list_todos = sg.Listbox(values=functions.get_todo_list(),
                         key="todos", enable_events=True, size=[45, 10],
                         background_color="blue")
 edit_button = sg.Button("Edit")
+remove_button = sg.Button("Remove")
+exit_button = sg.Button("Exit")
 
 
-layout = [[label], [input_box, add_button], [list_todos, edit_button]]
+layout = [[label], [input_box, add_button], [
+    list_todos, edit_button, remove_button], [exit_button]]
 
 window = sg.Window("My to do App", layout, font=("Helvetica", 20))
 
@@ -23,8 +26,9 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todo_list()
-            new_todo = values["todo"] + "\n"
+            new_todo = values["todo"] + '\n'
             todos.append(new_todo)
+            index_of_new_todo = todos.index(new_todo)
             functions.write_todo_to_list(todos)
             window['todos'].Update(values=todos)
 
@@ -37,8 +41,19 @@ while True:
             functions.write_todo_to_list(todos)
             window['todos'].Update(values=todos)
 
+        case "Remove":
+            todo_to_remove = values["todos"][0]
+            todos = functions.get_todo_list()
+            todos.remove(todo_to_remove)
+            functions.write_todo_to_list(todos)
+            window['todos'].Update(values=todos)
+            window['todo'].Update(value='')
+
         case 'todos':
             window['todo'].Update(value=values['todos'][0])
+
+        case 'Exit':
+            break
 
     if event in (None, sg.WIN_CLOSED):
         break
