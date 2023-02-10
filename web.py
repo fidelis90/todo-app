7cf88df634cd50858bae9dfe9e862e292ac4bff4
fidelis90 <fidelisogunsanmi@gmail.com>
2023-02-10 15:25:39 -0500

    add the "add todo item" function to web.py

--- web.py
+++ web.py
@@ -1,13 +1,26 @@
 import streamlit as st
 import functions as fc
 
+#: define function to add
+
+session_state = st.session_state
 todos = fc.get_todo_list()
 
-st.title('My Todo App')
-st.subheader("This is my todo app.")
-st.write('This is to increase my productivity daily')
+
+def add_todo_item():
+    todo = session_state["new_todo"] + '\n'
+    todos.append(todo)
+    fc.write_todo_to_list(todos)
+
+
+title = st.title('My Todo App')
+subheader = st.subheader("This is my todo app.")
+app_desc = st.write('This is to increase my productivity daily')
 
 for todo in todos:
-    st.checkbox(todo)
+    st.checkbox(todo, key='todo_item')
 
-st.text_input(label='', placeholder="Add an item")
+user_input = st.text_input(label='Enter todo item', label_visibility="hidden", placeholder="Add an item",
+                           key="new_todo", on_change=add_todo_item)
+
+session_state
